19e89277c127dc615eb6d7f860c7a7cc7e36078d
sujey22 <da0089032022@unab.edu.sv>
2023-02-25 07:58:46 -0600

    creando programa

---
+++ NumPares/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Ingrese el valor inicial: ");
+int inicio = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor final: ");
+int fin = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Los numeros pares en el rango son: ");
+
+for (int i = inicio; i <= fin; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write("{0} ", i);
+    }
+}
